b7afd2e99dafe80723be9e2a7edf38b5e0f78d54
Alberto Bor <alberto.bor@edu.unito.it>
2025-04-25 14:56:19 +0200

    aggiunta file b e fix

--- CardLineari.js
+++ CardLineari.js
@@ -10,7 +10,7 @@ cardinali=[
         "continente": "Europa",
         "wikiUrl": "https://it.wikipedia.org/wiki/Matteo_Maria_Zuppi",
         "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Matteo_Zuppi_2019.jpg/250px-Matteo_Zuppi_2019.jpg",
-        "punteggio": 38
+        "punteggio": 20
     },
     {
         "nome": "Pierbattista Pizzaballa",
@@ -21,18 +21,18 @@ cardinali=[
         "continente": "Asia",
         "wikiUrl": "https://it.wikipedia.org/wiki/Pierbattista_Pizzaballa",
         "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Mons._Pierbattista_Pizzaballa.jpg/250px-Mons._Pierbattista_Pizzaballa.jpg",
-        "punteggio": 35
+        "punteggio": 19
     },
     {
-        "id": 1,
         "nome": "Luis Antonio Tagle",
-        "eta": 67,
+        "dataNascita": "21/06/1957",
         "paese": "Filippine",
         "ruolo": "Prefetto del Dicastero per l'Evangelizzazione",
+        "dataNomina": "24/11/2012",
         "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Cardinal_Luis_Antonio_Tagle_Sambuhay_2019.jpg/250px-Cardinal_Luis_Antonio_Tagle_Sambuhay_2019.jpg",
         "continente": "Asia",
         "wikiUrl": "https://it.wikipedia.org/wiki/Luis_Antonio_Tagle",
-        "punteggio": 34
+        "punteggio": 18
     },
     {
         "nome": "Pietro Parolin",
@@ -43,7 +43,7 @@ cardinali=[
         "continente": "Europa",
         "wikiUrl": "https://it.wikipedia.org/wiki/Pietro_Parolin",
         "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/In%C4%81ra_M%C5%ABrniece_tiekas_ar_Sv%C4%93t%C4%81_Kr%C4%93sla_Valsts_sekret%C4%81ru_%2826363562894%29-1%28cropped%29.jpg/250px-In%C4%81ra_M%C5%ABrniece_tiekas_ar_Sv%C4%93t%C4%81_Kr%C4%93sla_Valsts_sekret%C4%81ru_%2826363562894%29-1%28cropped%29.jpg",
-        "punteggio": 33
+        "punteggio": 17
     },
     {
         "nome": "Fridolin Ambongo Besungu",
@@ -54,7 +54,7 @@ cardinali=[
         "continente": "Africa",
         "wikiUrl": "https://it.wikipedia.org/wiki/Fridolin_Ambongo_Besungu",
         "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/AED_11%C3%A8me_Nuit_des_T%C3%A9moins_-_Fridolin_Ambongo_-_2_%28cropped%29.jpg/250px-AED_11%C3%A8me_Nuit_des_T%C3%A9moins_-_Fridolin_Ambongo_-_2_%28cropped%29.jpg",
-        "punteggio": 32
+        "punteggio": 17
     },
     {
         "nome": "Jean-Claude Hollerich",
@@ -65,7 +65,7 @@ cardinali=[
         "continente": "Europa",
         "wikiUrl": "https://it.wikipedia.org/wiki/Jean-Claude_Hollerich",
         "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Jean-Claude_Hollerich.jpg/250px-Jean-Claude_Hollerich.jpg",
-        "punteggio": 30
+        "punteggio": 16
     },
     {
         "nome": "Péter Erdő",
@@ -76,7 +76,7 @@ cardinali=[
         "continente": "Europa",
         "wikiUrl": "https://it.wikipedia.org/wiki/P%C3%A9ter_Erd%C5%91",
         "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/P%C3%A9ter_Erd%C5%91_in_2011.jpg/250px-P%C3%A9ter_Erd%C5%91_in_2011.jpg",
-        "punteggio": 26
+        "punteggio": 14
     },
     {
         "nome": "Willem Jacobus Eĳk",
@@ -87,7 +87,7 @@ cardinali=[
         "continente": "Europa",
         "wikiUrl": "https://it.wikipedia.org/wiki/Willem_Jacobus_Eijk",
         "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Willem_Eijk_%282020%29.jpg/250px-Willem_Eijk_%282020%29.jpg",
-        "punteggio": 25
+        "punteggio": 14
     },
     {
         "nome": "Peter Kodwo Appiah Turkson",
@@ -98,7 +98,7 @@ cardinali=[
         "continente": "Africa",
         "wikiUrl": "https://it.wikipedia.org/wiki/Peter_Kodwo_Appiah_Turkson",
         "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Peter_Turkson_%28cropped%29.jpg/250px-Peter_Turkson_%28cropped%29.jpg",
-        "punteggio": 24
+        "punteggio": 13
     },
     {
         "nome": "Carlos Aguiar Retes",
@@ -109,7 +109,7 @@ cardinali=[
         "continente": "America del Nord",
         "wikiUrl": "https://it.wikipedia.org/wiki/Carlos_Aguiar_Retes",
         "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Cardenalaguiar.jpg/330px-Cardenalaguiar.jpg",
-        "punteggio": 23
+        "punteggio": 13
     },
     {
         "nome": "George Jacob Koovakad",
@@ -120,7 +120,7 @@ cardinali=[
         "continente": "Asia",
         "wikiUrl": "https://it.wikipedia.org/wiki/George_Jacob_Koovakad",
         "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Card._George_J._Koovakad.jpg/250px-Card._George_J._Koovakad.jpg",
-        "punteggio": 22
+        "punteggio": 12
     },
     {
         "nome": "Dominique Mamberti",
@@ -131,7 +131,7 @@ cardinali=[
         "continente": "Europa",
         "wikiUrl": "https://it.wikipedia.org/wiki/Dominique_Mamberti",
         "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Dominique_Mamberti%2C_14_Dec_2010.jpg/250px-Dominique_Mamberti%2C_14_Dec_2010.jpg",
-        "punteggio": 22
+        "punteggio": 12
     },
     {
         "nome": "Blase Joseph Cupich",
@@ -142,7 +142,7 @@ cardinali=[
         "continente": "America del Nord",
         "wikiUrl": "https://it.wikipedia.org/wiki/Blase_Joseph_Cupich",
         "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Cdl._Cupich_%28cropped%29.png/250px-Cdl._Cupich_%28cropped%29.png",
-        "punteggio": 21
+        "punteggio": 11
     },
     {
         "nome": "Baldassare Reina",
@@ -153,7 +153,7 @@ cardinali=[
         "continente": "Europa",
         "wikiUrl": "https://it.wikipedia.org/wiki/Baldassare_Reina",
         "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Baldassare_Reina.jpg/330px-Baldassare_Reina.jpg",
-        "punteggio": 21
+        "punteggio": 11
     },
     {
         "nome": "Anders Arborelius",
@@ -164,7 +164,7 @@ cardinali=[
         "continente": "Europa",
         "wikiUrl": "https://it.wikipedia.org/wiki/Anders_Arborelius",
         "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Anders_Arborelius_in_2019.jpg/250px-Anders_Arborelius_in_2019.jpg",
-        "punteggio": 20
+        "punteggio": 11
     },
     {
         "nome": "Charles Maung Bo",
@@ -175,7 +175,7 @@ cardinali=[
         "continente": "Asia",
         "wikiUrl": "https://it.wikipedia.org/wiki/Charles_Maung_Bo",
         "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Maung_Bo.jpg/330px-Maung_Bo.jpg",
-        "punteggio": 19
+        "punteggio": 10
     },
     {
         "nome": "Albert Malcolm Ranjith Patabendige Don",
@@ -186,7 +186,7 @@ cardinali=[
         "continente": "Asia",
         "wikiUrl": "https://it.wikipedia.org/wiki/Albert_Malcolm_Ranjith_Patabendige_Don",
         "imageUrl": "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRyYhpn3XPrr-jaebDKuGRC6YcN6TClYt-QzlXbyTRuoXxpDfQD0s_Bcf7TXFRCQl8_-Fiex3q6HWrj73LH7UyaLA",
-        "punteggio": 18
+        "punteggio": 9
     },
     {
         "nome": "Wilton Daniel Gregory",
@@ -197,7 +197,7 @@ cardinali=[
         "continente": "America del Nord",
         "wikiUrl": "https://it.wikipedia.org/wiki/Wilton_Daniel_Gregory",
         "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Cardinal_Wilton_Daniel_Gregory_in_2024_%28cropped%29.jpg/250px-Cardinal_Wilton_Daniel_Gregory_in_2024_%28cropped%29.jpg",
-        "punteggio": 17
+        "punteggio": 9
     },
     {
         "nome": "Giuseppe Betori",
@@ -208,7 +208,7 @@ cardinali=[
         "continente": "Europa",
         "wikiUrl": "https://it.wikipedia.org/wiki/Giuseppe_Betori",
         "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Betori.JPG/250px-Betori.JPG",
-        "punteggio": 16
+        "punteggio": 8
     },
     {
         "nome": "Fernando Vérgez Alzaga",
@@ -219,7 +219,7 @@ cardinali=[
         "continente": "Europa",
         "wikiUrl": "https://it.wikipedia.org/wiki/Fernando_V%C3%A9rgez_Alzaga",
         "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Fernando_V%C3%A9rgez_Alzaga%2C_posesi%C3%B3n_Cobo-Roma.jpg/500px-Fernando_V%C3%A9rgez_Alzaga%2C_posesi%C3%B3n_Cobo-Roma.jpg",
-        "punteggio": 15
+        "punteggio": 7
     },
     {
         "nome": "Mario Grech",
@@ -230,7 +230,7 @@ cardinali=[
         "continente": "Europa",
         "wikiUrl": "https://it.wikipedia.org/wiki/Mario_Grech",
         "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Cardinal_Mario_Grech.jpg/250px-Cardinal_Mario_Grech.jpg",
-        "punteggio": 15
+        "punteggio": 7
     },
     {
         "nome": "Robert Francis Prevost",
@@ -241,7 +241,7 @@ cardinali=[
         "continente": "America del Nord",
         "wikiUrl": "https://it.wikipedia.org/wiki/Robert_Francis_Prevost",
         "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Mons._Robert_Prevost%2C_OSA_%28cropped%29.jpg/250px-Mons._Robert_Prevost%2C_OSA_%28cropped%29.jpg",
-        "punteggio": 15
+        "punteggio": 7
     },
     {
         "nome": "Ángel Fernández Artime",
@@ -252,7 +252,7 @@ cardinali=[
         "continente": "Europa",
         "wikiUrl": "https://it.wikipedia.org/wiki/%C3%81ngel_Fern%C3%A1ndez_Artime",
         "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/%C3%81ngel_Fern%C3%A1ndez_Artime_%281960-%29%2C.jpg/250px-%C3%81ngel_Fern%C3%A1ndez_Artime_%281960-%29%2C.jpg",
-        "punteggio": 15
+        "punteggio": 8
     },
     {
         "nome": "Robert Sarah",
@@ -263,7 +263,7 @@ cardinali=[
         "continente": "Africa",
         "wikiUrl": "https://it.wikipedia.org/wiki/Robert_Sarah",
         "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Cardinal_Robert_Sarah_%28cropped%29.JPG/250px-Cardinal_Robert_Sarah_%28cropped%29.JPG",
-        "punteggio": 15
+        "punteggio": 7
 
     },
     {
@@ -275,7 +275,7 @@ cardinali=[
         "continente": "Europa",
         "wikiUrl": "https://it.wikipedia.org/wiki/Jean-Marc_Aveline",
         "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Mgr_Jean-Marc_Aveline_par_Claude_Truong-Ngoc_juillet_2021.jpg/500px-Mgr_Jean-Marc_Aveline_par_Claude_Truong-Ngoc_juillet_2021.jpg",
-        "punteggio": 15
+        "punteggio": 7
     },
     {
         "nome": "Daniel Fernando Sturla Berhouet",
@@ -286,7 +286,7 @@ cardinali=[
         "continente": "America del Sud",
         "wikiUrl": "https://it.wikipedia.org/wiki/Daniel_Fernando_Sturla_Berhouet",
         "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Daniel_Sturla_en_Misa_0013.jpg/330px-Daniel_Sturla_en_Misa_0013.jpg",
-        "punteggio": 15
+        "punteggio": 7
     },
     {
         "nome": "Thomas Aquino Manyo Maeda",
@@ -297,7 +297,7 @@ cardinali=[
         "continente": "Asia",
         "wikiUrl": "https://it.wikipedia.org/wiki/Thomas_Aquino_Manyo_Maeda",
         "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Archbishop_Thomas_Aquinas_Manyo_Maeda_in_2015.jpg/225px-Archbishop_Thomas_Aquinas_Manyo_Maeda_in_2015.jpg",
-        "punteggio": 15
+        "punteggio": 7
     },
     {
         "nome": "Leonardo Ulrich Steiner",
@@ -308,7 +308,7 @@ cardinali=[
         "continente": "America del Sud",
         "wikiUrl": "https://it.wikipedia.org/wiki/Leonardo_Ulrich_Steiner",
         "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/CDH_-_Comiss%C3%A3o_de_Direitos_Humanos_e_Legisla%C3%A7%C3%A3o_Participativa_%2821412066008%29.jpg/250px-CDH_-_Comiss%C3%A3o_de_Direitos_Humanos_e_Legisla%C3%A7%C3%A3o_Participativa_%2821412066008%29.jpg",
-        "punteggio": 15
+        "punteggio": 7
     },
 
 ]
