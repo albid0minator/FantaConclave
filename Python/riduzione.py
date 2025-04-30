b4bc6b9592b88ee1bd5c7593459b483d6a4d5f63
Alberto Bor <alberto.bor@edu.unito.it>
2025-04-30 08:53:38 +0200

    Aggiunta ricerca

--- Python/riduzione.py
+++ Python/riduzione.py
@@ -51,5 +51,5 @@ def seleziona_colonne_specifiche(input_csv_file, output_csv_filename="output_col
         print(f"Si è verificato un errore: {e}")
 
 if __name__ == "__main__":
-    input_file = "Aggiorna.csv"
+    input_file = "852.csv"
     seleziona_colonne_specifiche(input_file)
